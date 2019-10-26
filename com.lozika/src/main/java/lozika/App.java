1bc66e2ffa75c4c24bd51bb6a9b6c4ec9411c5d6
henimbola <henimbola@gmail.com>
2019-10-26 13:43:47 +0300

    Trying to make the sequential matrix multiplication work. But it's not really working now.

--- com.lozika/src/main/java/lozika/App.java
+++ com.lozika/src/main/java/lozika/App.java
@@ -1,5 +1,7 @@
 package lozika;
 
+import lozika.multmatrix.MultmatSeq;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        seqMultiply();
+    }
+
+    public static void seqMultiply() {
+        MultmatSeq multmatSeq = new MultmatSeq();
+        Long start = System.currentTimeMillis();
+        multmatSeq.multiply();
+        Long end = System.currentTimeMillis();
+        System.out.print( "Time elapsed : " + (end - start) );
     }
 }
--- com.lozika/src/main/java/lozika/multmatrix/MultmatSeq.java
+++ com.lozika/src/main/java/lozika/multmatrix/MultmatSeq.java
@@ -9,23 +9,34 @@ public class MultmatSeq {
 
     public MultmatSeq() {
         Random rd = new Random();
-
+        System.out.println("A : ");
         for(int i=0; i<A.length; i++) {
             for(int j = 0; j<A[i].length; j++) {
                 A[i][j] = rd.nextInt(15);
+                System.out.print(A[i][j] + " ");
             }
+            System.out.println("");
         }
+
+        System.out.println("");
+        System.out.println("B : ");
 
         for(int i=0; i<B.length; i++) {
             for(int j = 0; j<B[i].length; j++) {
                 B[i][j] = rd.nextInt(15);
+                System.out.print(B[i][j] + " ");
             }
+            System.out.println("");
         }
+
+        System.out.println("\n\n");
+
     }
 
     public int[][] multiply() {
-        int[][] C = new int[this.A.length][this.B[15].length];
-        
+
+        int[][] C = new int[this.A.length][this.B[0].length];
+    
         for(int row = 0; row < C.length; row++) {
             for(int col = 0; row < C[row].length; row++) {
                 int cell = 0;
@@ -33,9 +44,13 @@ public class MultmatSeq {
                     cell += A[row][i] * B[i][col];
                 }
                 C[row][col] = cell;
+                System.out.print(C[row][col] + " ");
             }
+            //System.out.println("");
         }
         
+        System.out.println("\n\n");
+
         return C;
     }
 }
